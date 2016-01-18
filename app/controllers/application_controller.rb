1a1cb6fee442121fb666ebff734c2894806d8674
buyantorgil <enkhbaatar_buyant-orgil@unimedia.co.jp>
2016-01-18 12:59:45 +0800

    Added mui style on devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :layout_by_resource
+
+protected
+
+  def layout_by_resource
+    if devise_controller? && (action_name == "new" || action_name == "create")
+      "application"
+    else
+      "dashboard"
+    end
+  end
 end
