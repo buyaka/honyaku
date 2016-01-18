4b85aaaf4d85ff47383e6206e09c817aa48a8ffb
buyantorgil <enkhbaatar_buyant-orgil@unimedia.co.jp>
2016-01-18 13:23:50 +0800

    translate pagination fix

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,7 +1,7 @@
 module PagesHelper
 
   def prePage(page_id)
-    page = Page.where(" id < #{page_id} ").first
+    page = Page.where(" pagenumber < #{page_id} ").order("pagenumber desc").first
     if page != nil
       return link_to("<<前へ", translate_path(@book, page))
     end
@@ -9,7 +9,7 @@ module PagesHelper
   end
 
   def nextPage(page_id)
-    page = Page.where(" id > #{page_id} ").first
+    page = Page.where(" pagenumber > #{page_id} ").order("pagenumber asc").first
     if page != nil
       return link_to("次へ>>", translate_path(@book, page))
     end
